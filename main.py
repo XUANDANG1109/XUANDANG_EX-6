ec003076870c5b88a0c9b725d5ba3b850a51fb29
XUANDANG1109 <dangthilinhxuan@gmail.com>
2022-09-10 22:11:53 +0300

    ee

---
+++ main.py
@@ -0,0 +1,20 @@
+# NAME: XUAN DANG
+
+# Phase 1
+import random
+
+def dice_roll():
+    a = random.randint(1,6)
+    return a
+number = dice_roll()
+while number !=6:
+    print(number)
+    number = dice_roll()
+print(number)
+
+# Phase 2
+import random
+def dice_roll():
+    number = int(input("Sides the dice:")
+    a = random.randint(1,n)
+while
